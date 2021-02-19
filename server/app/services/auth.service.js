542be0ddcefc40f2f8487330c5e9c30f3473b019
shubhangik1511 <shubhangik1511@gmail.com>
2021-02-19 20:25:45 +0530

    Removed password hashing for now.

--- server/app/services/auth.service.js
+++ server/app/services/auth.service.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const db = require("../models");
 const TokenBlacklistService = require('../services/token-blacklist.service');
 const User = db.userLogin;
@@ -9,10 +9,10 @@ exports.signup = async (data) => {
     if (!data.username || !data.password) {
         throw new CustomError("All fields are required.");
     }
-    const hash = await bcrypt.hashSync(data.password, 10);
+    // const hash = await bcrypt.hashSync(data.password, 10);
     const response = await User.create({
         username: data.username,
-        password: hash
+        password: data.password
     });
     return response;
 }
@@ -20,8 +20,8 @@ exports.signup = async (data) => {
 exports.signin = async (data) => {
     const user = await User.findOne({ where: { username: data.username } });
     if (user && user.username && user.id) {
-        const valid = await bcrypt.compareSync(data.password, user.password);
-        if (valid) {
+        // const valid = await bcrypt.compareSync(data.password, user.password);
+        if (user.password == data.password) {
             const token = jwt.sign(
                 {
                     id: user.id,
